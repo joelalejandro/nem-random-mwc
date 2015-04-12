dee9beaed87a52eed66b298d2777e8591ae595cc
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2015-04-12 16:57:13 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomMwc = module.exports = function(options) {
+  var defaultOptions = { 
+    seeds: [
+      new Date().valueOf(), 
+      parseInt(new Date().valueOf().toString().split("").reverse().join(""))
+    ],
+    includeNegativeNumbers: false
+  };
+  options = options || defaultOptions;
+  var mw = options.seeds[0], mz = options.seeds[1];
+  mw = 36969 * (mz & 65535) + (mz >> 16);
+  mz = 18000 * (mw & 65535) + (mw >> 16);
+  var result = parseFloat("0." + Math.abs(((mz << 16) + mw)));
+  if (options.includeNegativeNumbers) {
+    if (Math.random() >= 0.5) {
+      result *= -1;
+    }
+  }
+  return result;
+}
